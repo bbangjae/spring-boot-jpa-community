b8298f53c1f1174456513f2b1f6a45a75f88fe8a
bbangjae <thebyunjae@naver.com>
2023-12-14 22:45:20 +0900

    라이브러리(lombok) 추가 , getter setter 설정

--- src/main/java/springStudy/springCommunity/domain/Community.java
+++ src/main/java/springStudy/springCommunity/domain/Community.java
@@ -4,30 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity
+@Entity @Setter @Getter
 public class Community {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
-
-    public void setTitle(String name) {
-        this.title = title;
-    }
 }
 
 
